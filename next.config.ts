ef51753f811e06909b9080fa383b78afb348af9b
OvanessTea <OvanessTea@yandex.com>
2025-04-08 22:41:20 +0200

    set up configs

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  images: {
+    domains: ['test-task-api.allfuneral.com'],
+  },
+};
+
+export default nextConfig;
